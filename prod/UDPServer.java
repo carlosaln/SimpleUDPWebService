3c9f149fa6ec754654fbea45a867cf6313be118e
Carlos Lemus <cal0018@auburn.edu>
2014-06-27 12:07:11 -0500

    Full implementation of UDPServer side (untested)

--- prod/UDPServer.java
+++ prod/UDPServer.java
@@ -1,44 +1,100 @@
 package prod;
 
 import java.net.*;
+import java.io.FileInputStream;
+import java.io.File;
+
 import prod.HTTPMethod;
 
 public class UDPServer {
-	
-	public void handleHTTPMethod(HTTPMethod method, String data) {
-		switch (method) {
+
+	public void handlePackets(DatagramSocket socket, byte[] sendBuffer,
+			byte[] receiveBuffer) throws Exception {
+
+		DatagramPacket receivePacket = new DatagramPacket(receiveBuffer,
+				receiveBuffer.length);
+		socket.receive(receivePacket);
+
+		String receiveMessage = new String(receivePacket.getData());
+		InetAddress clientIPAddress = receivePacket.getAddress();
+		int clientPort = receivePacket.getPort();
+
+		switch (getHTTPMethod(receiveMessage)) {
 		case GET:
-			System.out.println("Get");
+			/* Send initial header including file size */
+			File requestedFile = getAssociatedFile(receiveMessage);
+
+			// Potential for sending error header if file does not exist
+			String header = generateResponseHeader(requestedFile.length());
+			sendBuffer = header.getBytes();
+
+			DatagramPacket sendPacket = new DatagramPacket(sendBuffer,
+					sendBuffer.length, clientIPAddress, clientPort);
+
+			socket.send(sendPacket);
+
+			/* Send all bytes until end of file */
+			FileInputStream fin = new FileInputStream(requestedFile);
+
+			while (fin.read(sendBuffer) != -1) {
+				sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length,
+						clientIPAddress, clientPort);
+
+				socket.send(sendPacket);
+			}
+
+			/* Send null character */
+			byte[] nullEnd = new byte[1];
+			nullEnd[0] = 0;
+
+			sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length,
+					clientIPAddress, clientPort);
+
+			socket.send(sendPacket);
 			break;
 		default:
+			//potential sending error if method is invalid
 			throw new java.lang.IllegalArgumentException();
 		}
 	}
-	
-	public HTTPMethod parseHTTPMessage(String message) {
-		String tokens[] =  message.split(" ");
-		
+
+	public HTTPMethod getHTTPMethod(String message) {
+		String tokens[] = message.split("\\s+");
+
 		if (tokens[0].compareTo("GET") == 0) {
 			return HTTPMethod.GET;
-		}
-		else {
+
+		} else {
 			return HTTPMethod.BAD;
 		}
 	}
-	
-	public String generateResponseHeader(int contentLength) {
+
+	public File getAssociatedFile(String message)
+			throws IllegalArgumentException {
+		String tokens[] = message.split("\\s+");
+
+		if (tokens[0].compareTo("GET") == 0) {
+			String fileName = tokens[1];
+			File retFile = new File(fileName);
+			return retFile;
+		} else {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	public String generateResponseHeader(long contentLength) {
 		String result = new String();
-		
+
 		result += "HTTP 1.0 ";
 		result += "200 ";
 		result += "Document Follows\r\n";
-		
+
 		result += "Content-Type: ";
 		result += ("text/plain\r\n");
 		result += ("Content-Length: " + contentLength + "\r\n");
 		result += ("\r\n");
-		
+
 		return result;
 	}
-	
+
 }
--- prod/UDPServerMain.java
+++ prod/UDPServerMain.java
@@ -1,47 +1,22 @@
 package prod;
 
+import java.net.*;
 import prod.UDPServer;
 
 public class UDPServerMain {
 	
-	// this should be consistenly running on server side.
+	final static int bufferSize = 128;
+
+	// this should be consistently running on server side.
 	public static void main(String[] args) throws Exception {
-//		DatagramSocket serverSocket = new DatagramSocket(10034);
-//
-//		byte[] receiveData = new byte[1024];
-//		byte[] sendData = new byte[1024];
-//
-//		while (true) {
-//
-//			DatagramPacket receivePacket = new DatagramPacket(receiveData,
-//					receiveData.length);
-//
-//			System.out.println("About to receive data Packet.");
-//
-//			serverSocket.receive(receivePacket);
-//
-//			String sentence = new String(receivePacket.getData());
-//
-//			InetAddress IPAddress = receivePacket.getAddress();
-//
-//			int port = receivePacket.getPort();
-//			
-//			//start changing here
-//			String capitalizedSentence = sentence.toUpperCase();
-//
-//			sendData = capitalizedSentence.getBytes();
-//
-//			DatagramPacket sendPacket = new DatagramPacket(sendData,
-//					sendData.length, IPAddress, port);
-//
-//			serverSocket.send(sendPacket);
-			
-//		}
-		
+
 		UDPServer server = new UDPServer();
+		DatagramSocket serverSocket = new DatagramSocket(10034);
+		byte[] sendBuffer = new byte[bufferSize];
+		byte[] receiveBuffer = new byte[bufferSize];
 		
-		String rHeader = server.generateResponseHeader(200);
-		
-		System.out.print(rHeader);
+		while (true) {
+			server.handlePackets(serverSocket, sendBuffer, receiveBuffer);
+		}
 	}
 }
